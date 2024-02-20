97c438658eeed0045594cb5eaddee0781cc74ecc
Ranjana S <ranjanasaye@gmail.com>
2024-02-20 19:43:08 +0530

    added new changes

--- integrate/src/App.js
+++ integrate/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Container, Segment } from 'semantic-ui-react';
 import { useSpeechSynthesis } from 'react-speech-kit';
 import Tesseract from 'tesseract.js';
+import './styles.css';
 
 function App() {
   const [file, setFile] = useState();
@@ -44,31 +45,42 @@ function App() {
   return (
     <div className="App">
       <h1>Text to speech converter in React</h1>
-      <input type="file" onChange={onFileChange} />
+      <input type="file" onChange={onFileChange} className='fileInput'/>
       <select
         value={language}
         onChange={(e) => setLanguage(e.target.value)}
+        className='selectStyle'
       >
         <option value="eng">English</option>
         <option value="tel">Telugu</option>
         <option value="hin">Hindi</option>
         <option value="kan">Kannada</option>
       </select>
-      <div style={{ marginTop: 25 }}>
-        <input type="button" value="Submit" onClick={processImage} />
+      <div style={{ marginTop: 25,marginLeft:10,textAlign:'center' }}>
+        <input type="button" value="Submit" onClick={processImage}  style={{
+      backgroundColor: '#007bff',
+      color: 'white',
+      border: 'none',
+      borderRadius: '5px',
+      padding: '10px 20px',
+      fontSize: '16px',
+      cursor: 'pointer',
+      justifyContent:'center',
+
+    }} />
       </div>
       <Container>
         <Segment> 
-          <button className="buttonStyle" onClick={handlePlayPause}>
+          <button className='buttonStyle' onClick={handlePlayPause}>
             {isPlaying ? 'Pause' : 'Play'}
           </button>
         </Segment>
       </Container>
-      <div>
-        <progress value={progress} max={1} />
+      <div style={{marginTop:20,width:'60%',marginLeft:'740px'}}>
+        <progress value={progress} max={1} style={{width:'25%',height:'20px',borderRadius:'5px',backgroundColor:'#f3f3f3'}} />
       </div>
       {result !== '' && (
-        <div style={{ marginTop: 20, fontSize: 18, color: 'teal' }}>
+        <div style={{ marginTop: 20, fontSize: 18, color: 'teal',marginLeft:'40px' }}>
           Result: {result}
         </div>
       )}
